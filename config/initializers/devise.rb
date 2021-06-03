855173ed31ba6b1486d5cd00e77272182120c933
Kate Syrmakesi <kate.syrmakesi@gmail.com>
2021-06-03 19:08:09 +0200

    bug fixes with log in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,6 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
-
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
